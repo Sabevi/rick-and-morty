36c90bf82e398af76fd12b5c83ce38ab0fcaede2
Zlahrouni <ziad.lahrouni@gmail.com>
2024-02-20 12:24:54 +0100

    Setting up a home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,45 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
+import CharacterDetails from "./component/characterDetails.tsx";
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
     const casts = [
         {
+            id: 1,
             name: "Amish Cyborg",
             image: "https://rickandmortyapi.com/api/character/avatar/16.jpeg"
         },
         {
+            id: 17,
             name: "Annie",
             image: "https://rickandmortyapi.com/api/character/avatar/17.jpeg"
         },
         {
+            id: 18,
             name: "Antenna Morty",
             image: "https://rickandmortyapi.com/api/character/avatar/18.jpeg"
         },
         {
+            id: 19,
             name: "Antenna Rick",
             image: "https://rickandmortyapi.com/api/character/avatar/19.jpeg"
         },
 
     ];
 
-    const castList = casts.map((cast) => (
-        <tr key={cast.name}>
-            <td>{cast.name}</td>
-        </tr>
-    ));
   return (
-    <>
-        <div>
-            <table>
-                <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Image</th>
-                </tr>
-                </thead>
-                <tbody>{castList}</tbody>
-            </table>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-            <button onClick={() => setCount((count) => count + 1)}>
-                count is {count}
-            </button>
-            <p>
-                Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="container">
+          <div className="row">
+              {casts.map((cast, index) => (
+                  <div className="col-md-3" key={index}>
+                      <CharacterDetails character={cast}/>
+                  </div>
+              ))}
+          </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
